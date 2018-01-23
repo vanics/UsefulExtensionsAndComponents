9952d1589b53ca1a059dc92173be0a3d589181f2
Icaro Oliveira <icaro@vanics.com>
2018-01-22 19:42:37 -0800

    Util to bind data

---
+++ UsefulExtensionsAndComponents/BindingData.swift
@@ -0,0 +1,34 @@
+//
+//  BindingData.swift
+//  UsefulExtensionsAndComponents
+//
+//  Created by Ícaro Oliveira on 22/01/18.
+//  Copyright © 2018 vanics. All rights reserved.
+//
+
+import Foundation
+
+class Dynamic<T> {
+    typealias Listener = (T) -> ()
+    var listener: Listener?
+    
+    func bind(_ listener: Listener?) {
+        self.listener = listener
+    }
+    
+    func bindAndFire(_ listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ v: T) {
+        value = v
+    }
+}
+
